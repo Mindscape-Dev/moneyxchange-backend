84c17515414a9a3ba6f06d59b97beb582197cc71
eminozkan <info@eminozkan.net>
2023-09-18 12:05:26 +0300

    add project files

---
+++ src/main/java/dev/mindscapedev/moneyxchange/MoneyxchangeApplication.java
@@ -0,0 +1,13 @@
+package dev.mindscapedev.moneyxchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyxchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyxchangeApplication.class, args);
+	}
+
+}

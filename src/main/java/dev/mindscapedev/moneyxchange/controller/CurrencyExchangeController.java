9482c0cba2d25c307fcd21d65bfb13df7d72473b
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-19 21:11:39 +0300

    add currency exchange controller

---
+++ src/main/java/dev/mindscapedev/moneyxchange/controller/CurrencyExchangeController.java
@@ -0,0 +1,11 @@
+package dev.mindscapedev.moneyxchange.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/currency")
+public class CurrencyExchangeController {
+
+
+}
